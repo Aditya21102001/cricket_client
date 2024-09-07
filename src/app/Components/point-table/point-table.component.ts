3046516f827ffd89066e8b52aa67b1529dd5266f
Aditya Yadav <adityakumaryadav21102001@gmail.com>
2024-09-08 01:24:19 +0530

    point table

--- src/app/Components/point-table/point-table.component.ts
+++ src/app/Components/point-table/point-table.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../../Services/api-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-point-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './point-table.component.html',
   styleUrl: './point-table.component.scss'
 })
-export class PointTableComponent {
+export class PointTableComponent implements OnInit {
+  pointTable: any;
+  ngOnInit(): void {
+    this.getPointTableData();
+  }
 
+  constructor(private apiService: ApiServiceService){}
+  getPointTableData(){
+    this.apiService.getPointTable().subscribe((res) =>{
+      this.pointTable=res;
+    })
+  }
 }
